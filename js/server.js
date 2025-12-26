7ee82f54a8e53b70437f3cf3fa4b4c0adaa9fe38
downstreamspiral <hemendrakumararya0@gmail.com>
2025-12-27 02:11:43 +0530

    express

--- js/server.js
+++ js/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static(__dirname));
 
 app.listen(3000, () => {
     console.log("Server running on http://localhost:3000");
