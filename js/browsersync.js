98e705faf84e63e0f8a4124b1e17f577828cf0b0
downstreamspiral <hemendrakumararya0@gmail.com>
2025-12-27 13:34:32 +0530

    uninstall

--- js/browsersync.js
+++
@@ -1,8 +0,0 @@
-const browserSync = require("browser-sync").create();
-
-browserSync.init({
-    proxy: "http://localhost:3000",
-    files: ["public/**/*.*"],
-    port: 4000,
-    open: true,
-});
