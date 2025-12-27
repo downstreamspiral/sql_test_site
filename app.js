01a1a4f7707d9c190c20129f59720beba55811ed
downstreamspiral <hemendrakumararya0@gmail.com>
2025-12-27 14:31:46 +0530

    folder_restructure

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 const express = require("express");
+const path = require('path');
 const app = express();
 
-
-app.get('/', (req, res) => {
-    res.send('GET request to the homepage')
-})
-
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, () => {
     console.log("Server running on http://localhost:3000");
